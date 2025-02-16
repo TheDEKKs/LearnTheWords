713469578a735c28bd0db0cf85a8f5ed68f84ed8
TheDEKKs <thedekkegor@gmail.com>
2025-02-16 21:48:18 +0300

    16.02.2025

---
+++ start.py
@@ -0,0 +1,40 @@
+import sys
+import json
+
+
+data = []
+
+def main():
+    # Проверяем, переданы ли аргументы
+    if len(sys.argv) > 1:
+        
+        data = sys.argv[1:]  
+        print("Переданные данные:", data)
+        file = data[0]
+        with open(f'{file}.json', 'r') as f:  # Открываем файл для чтения
+            loaded_array = json.load(f)
+            for i in range(len(loaded_array)):
+                text = loaded_array[i].split()
+                bol = False
+                while bol != True:
+                    answe = input(f"Введите перевод слова: {text[0]}: ")
+                    if answe == text[1]:
+                        print("Вы овтветили верно!")
+                        bol = True
+                    else: 
+                        print("Попробуйте ещё раз")
+                        continue
+
+            
+            print("Вы дали перевод всем вопросам!")
+
+            
+    else:
+        print("Не переданы данные.")
+
+
+
+
+if __name__ == "__main__":
+    main()
+
